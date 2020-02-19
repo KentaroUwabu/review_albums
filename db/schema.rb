0da854220f6cea53571a1504b023e0fe75a188ae
EC2 Default User <ec2-user@ip-172-31-35-93.ap-northeast-1.compute.internal>
2020-02-19 12:07:53 +0000

    データベース関連ファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_115238) do
+ActiveRecord::Schema.define(version: 2020_02_19_114048) do
+
+  create_table "albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
