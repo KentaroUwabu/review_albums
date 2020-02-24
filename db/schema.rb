4973050b899ec5c71e2fe954c9c7bf5ab356f235
EC2 Default User <ec2-user@ip-172-31-35-93.ap-northeast-1.compute.internal>
2020-02-24 07:37:33 +0000

    db関連ファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_095358) do
+ActiveRecord::Schema.define(version: 2020_02_24_065818) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 2020_02_21_095358) do
 
   create_table "albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
-    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
