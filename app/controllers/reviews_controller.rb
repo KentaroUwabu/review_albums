33cad77aed69454f6514bff85d7566e60395c67c
EC2 Default User <ec2-user@ip-172-31-35-93.ap-northeast-1.compute.internal>
2020-02-28 01:53:35 +0000

    reviewコントローラの編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   
   def new
+    @album = Album.find_by(id: params[:album_id])
     @review = Review.new
   end
   
@@ -9,8 +10,14 @@ class ReviewsController < ApplicationController
       title: params[:title],
       letter: params[:letter],
       rate: params[:rate],
+      album_id: @album.id,
       user_id: current_user.id
     )
+    if @review.save
+      redirect_to("/")
+    else
+      render("reviews/new")
+    end
   end
   
 end
